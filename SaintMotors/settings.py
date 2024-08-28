c944bb33d67b7b21e64db5e12c34ff3147db553e
work_penguin <d.sahakyan10@gmail.com>
2024-08-28 16:00:54 +0400

    cors settings1

--- SaintMotors/settings.py
+++ SaintMotors/settings.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
@@ -64,6 +64,15 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8010',
     'http://127.0.0.1:8001',
     'http://127.0.0.1:8010',
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
