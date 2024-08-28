8a6525163888a8dcb790a1fd0656b320289e1b82
work_penguin <d.sahakyan10@gmail.com>
2024-08-28 10:18:50 +0400

    collectstatic

--- SaintMotors/settings.py
+++ SaintMotors/settings.py
@@ -134,6 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
